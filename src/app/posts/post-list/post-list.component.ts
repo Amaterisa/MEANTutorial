4b4a000ded54f64f817c458e7fa0cce58ac5dcb2
Isabella Cordeiro <isabella@Isabellas-MacBook-Pro.local>
2021-07-06 21:17:44 -0400

    Implement initial code for mean course

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../post';
+import { PostService } from '../post.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSub!: Subscription;
+
+  constructor(public postsService: PostService) {
+  }
+
+  ngOnInit(): void {
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdatedListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  onDelete(postId: string){
+    this.postsService.deletePost(postId);
+  }
+
+  ngOnDestroy(): void {
+    this.postsSub.unsubscribe();
+  }
+}
